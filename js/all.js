0bf0f2b6e470f05099a2777129f5fb0e2d29f4e6
ivy2846357 <ivy2846357@icloud.com>
2022-05-06 11:04:10 +0800

    補註解，修改書本位置

--- js/all.js
+++ js/all.js
@@ -1,35 +1,43 @@
+// 自定義屬性
 var winWidth = $(window).width();
 var ratio = winWidth / 1920;
 var posX = Math.max(80 * ratio, 30);
 
+// 進下一頁
 $(".front:not(.last)").click(function(event) {
+  // 點擊放大鏡打開 PDF 不要進下一頁
   let target = $(event.target)
   if(target.is('.open-pdf')){
     retuen
   }
+  // 進下一頁
   $(".book").addClass("open").addClass("openbook")
   $(this).parent(".paper").addClass("open");
 });
 
+// 回上一頁
 $(".back").click(function(event) {
+  // 點擊放大鏡打開 PDF 不要回下一頁
   let target = $(event.target)
   if(target.is('.open-pdf')){
     retuen
   }
+  // 回翻時，如果點擊回第一頁的動作
   if ($(this).parent(".paper").index() == 0){
     $(".book").removeClass("open").removeClass("openbook");
     $(this).parent(".paper").removeClass("open");
   }else{
+    // 回上一頁
     $(this).parent(".paper").removeClass("open");
   }
 });
 
+// 點擊至最後一頁
 $('.teams-page-27').click(function(e){
-  $('.shadow').addClass('d-none');
-  $('.openbook').css('left', '70%');
+  $('.openbook').css('left', '60%');
 })
 
+// 從最後一頁翻回來
 $('.teams-page-28').click(function(){
-  $('.shadow').removeClass('d-none');
-  $('.openbook').css('left', '45%');
+  $('.openbook').css('left', '50%');
 })
